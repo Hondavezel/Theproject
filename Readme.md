8fe98eda12b9186335f2ff043e87d2feedcfcac2
unknown <twofahd@hotmail.com>
2020-12-13 08:59:09 +0500

    Random Message Generator

---
+++ Readme.md
@@ -0,0 +1,4 @@
+##The Message Project
+### This Project Displays a Random Message everytime its run
+
+It combines an array of messages to display a *message*
---
+++ Theproject.js
@@ -0,0 +1,21 @@
+const message1=['A day has','A year has','A week has'];
+const message2=['365 days','24 hours','7 days'];
+
+const messageMixer =(array1,array2)=>{
+    let messageArray =[];
+    for(key of array1){
+        if(key == 'A day has'){
+            messageArray[0] = key + ' ' + array2.find(element=> element == '24 hours');
+        }
+        if (key =='A year has'){
+            messageArray[1] = key + ' ' + array2.find(element=> element == '365 days');
+        }
+        if (key =='A week has'){
+            messageArray[2] = key + ' ' + array2.find(element=> element == '7 days');
+        }
+        
+    }
+    return messageArray[Math.floor(Math.random()* 3)];
+
+}
+console.log(messageMixer(message1,message2));
